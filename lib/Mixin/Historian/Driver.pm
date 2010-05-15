74c8f521842c83f164bee7565b11bbf92aff489a
Ricardo Signes <rjbs@cpan.org>
2010-05-15 14:38:12 -0400

    docs

--- lib/Mixin/Historian/Driver.pm
+++ lib/Mixin/Historian/Driver.pm
@@ -1,7 +1,23 @@
 use strict;
 use warnings;
 package Mixin::Historian::Driver;
+# ABSTRACT: base class for classes that act as Historian storage drivers
 
+=head1 METHODS
 
+Classes extending Mixin::Historian::Driver are expected to provide the
+following methods:
+
+=head1 new
+
+This method gets the driver configuration from the call to
+L<Mixin::Historian>'s C<import> method and should return a new driver instance.
+
+=head1 add_history
+
+This method is passed an arrayref of the argument(s) to the generated and
+installed C<add_history> method.  It is is expected to store the history entry.
+
+=cut
 
 1;
--- lib/Mixin/Historian/Driver/Array.pm
+++ lib/Mixin/Historian/Driver/Array.pm
@@ -2,6 +2,22 @@ use strict;
 use warnings;
 package Mixin::Historian::Driver::Array;
 use base 'Mixin::Historian::Driver';
+# ABSTRACT: a driver that stores history in an in-memory array (for testing)
+
+=head1 DESCRIPTION
+
+This driver, meant primarily for testing, logs history events as hashrefs in an
+in-memory arrayref stored in the driver.
+
+The events may accessed by the driver's C<entries> method, and are returned as
+a list of hashrefs in the form:
+
+  {
+    time   => $epoch_seconds,
+    record => $hashref_passed_to_add_history,
+  }
+
+=cut
 
 sub new {
   my ($class, $arg) = @_;
