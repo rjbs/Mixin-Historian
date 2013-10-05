b4f8e7a33b98a961d1099ba0bbf5204823438909
Ricardo Signes <rjbs@cpan.org>
2013-10-05 07:53:16 -0400

    more documentation

--- lib/Mixin/Historian.pm
+++ lib/Mixin/Historian.pm
@@ -14,6 +14,62 @@ use Sub::Exporter -setup => {
   installer => Sub::Exporter::ForMethods::method_installer(),
 };
 
+=head1 SYNOPSIS
+
+  package My::Object;
+  use Mixin::Historian -history => {
+    driver => {
+      class => 'YourDriver',
+      ...,
+    },
+  };
+
+  # Later...
+  my $object = My::Object->retrieve(1234);
+
+  $object->add_history({
+    type     => 'lava damage',
+    severity => 'very badly burned',
+    volcano  => 'Eyjafjallajokull',
+  });
+
+=head1 DESCRIPTION
+
+Mixin::Historian is an application of Mixin::ExtraFields.  If you're not
+familiar with it, you should read about it, both in L<its
+documentation|Mixin::ExtraFields> and in L<this article about
+Mixin::ExtraFields|http://advent.rjbs.manxome.org/2009-12-22.html>.
+
+Generally, it provides simple mechanism for write-only history.  Importing the
+C<-history> group will get you the C<add_history> method, which generally will
+accept one hashref with at least a C<type> key.  This will be passed along to
+the driver's C<add_history> method.
+
+=head1 TODO
+
+I have shoehorned an extra layer of functionality into the Historian driver
+that I use in my employer's code.  When initialized, the Historian mixin is
+told all legal types, something like this:
+
+  type_map => {
+    'lava damage' => {
+      severity => { required => 1, store_as => 'extra_1' },
+      volcano  => { required => 0, store_as => 'extra_2' },
+    },
+    ...
+  }
+
+This way, history entries can be validated before writing.  The C<store_as>
+entries indicate how the arguments to C<add_history> are mapped to database
+columns.  The entire argument is also stored in one field as JSON, and a few
+other attributes are always required (like C<by_whom>) and some are added just
+in time (like C<logged_at>).
+
+This feature is not yet present in the CPAN library because I have not yet
+found a suitable decomposition of concerns to make it a component.
+
+=cut
+
 sub default_moniker { 'history' }
 
 sub driver_base_class { 'Mixin::Historian::Driver' }
